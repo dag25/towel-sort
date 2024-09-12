80afb0f0ffb59e237d4eadaafb144ce446b41a5e
dag <45577604+dag25@users.noreply.github.com>
2024-09-12 21:39:51 +0400

    feat: solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+    // Flatten the matrix and sort it in reverse order
+
+    if (matrix === undefined) {
+        return [];
+    } else if(matrix.length === 0) {
+        return [];
+    } else {
+        const result = [];
+        matrix.forEach((value, index) => {
+            if (index % 2 !== 0) {
+                result.push(...value.reverse());
+            } else {
+                result.push(...value);
+            }
+        });
+        return result;
+    }
+};
